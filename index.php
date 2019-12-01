85c24233373c28a4b9fb140d6aa988d031c5e256
Alex Fleming <alexfleming421@gmail.com>
2019-11-30 23:02:41 -0600

    UX Layout - Stuck on Scroll Issue

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+/*****************************************************************************
+Title:  	Developer Wiki
+Use:     	Final Project - Store useful snippets related to development
+Author:  	Alex Fleming
+School:  	Southern Illinois University
+Term:    	Fall 2019
+Developed:  11/30/19
+Tested:     11/30/19
+******************************************************************************/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Developer Wiki</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+    <div>
+        <h2>
+            Welcome to Developer Wiki!
+        </h2>
+    </div>
+    <div>
+        <p>
+            Instructions...
+        </p>
+    </div>
+    <div>
+        <button>Snippets</button>
+        <button>Add Snippet</button>
+        <button>Pinned Snippets</button>
+    </div>
+
+</body>
+</html>
---
+++ pinned.php
@@ -0,0 +1,25 @@
+<?php
+
+/*****************************************************************************
+Title:  	Developer Wiki
+Use:     	Final Project - Store useful snippets related to development
+Author:  	Alex Fleming
+School:  	Southern Illinois University
+Term:    	Fall 2019
+Developed:  11/30/19
+Tested:     11/30/19
+******************************************************************************/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Email via PHP</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+
+</body>
+</html>
